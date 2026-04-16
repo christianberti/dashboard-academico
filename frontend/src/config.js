2338eca0c5ef23572e699d2285b32210192a6df3
Christian Berti <christiangberti@gmail.com>
2026-04-16 13:52:01 -0300

    Deployment: Switch to HTTPS and sync favicon update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 // Configuración global de la API
-const API_BASE_URL = "http://dashboard-academico.infinityfreeapp.com/backend"; 
+const API_BASE_URL = "https://dashboard-academico.infinityfreeapp.com/backend"; 
 
 export default API_BASE_URL;
+
 
